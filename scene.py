6b265854e06b52fdc5d135bb23fd2260b6240a8d
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2024-11-26 15:15:58 -0500

    nov 26 - 3:15 pm
    renders a red stripe idk why (sorta good lighting tho!)

--- scene.py
+++ scene.py
@@ -49,10 +49,6 @@ class Scene:
         u = glm.normalize(u)
         v = glm.cross(w, u)
 
-        print("w : ", w)
-        print("u : ", u)
-        print("v : ", v)
-
         for col in tqdm(range(self.width)):
             for row in range(self.height):
 
@@ -63,8 +59,8 @@ class Scene:
                 pixelX = ((col + 0.5)/self.width) * (right - left) + left
                 pixelY = ((row + 0.5)/self.height) * (top - bottom) + bottom
 
-                print("pixelX : ", pixelX)
-                print("pixelY : ", pixelY)
+                # print("pixelX : ", pixelX)
+                # print("pixelY : ", pixelY)
 
                 # Calculating the position in 3D of the pixel (in camera coordinates)
                 s = e + pixelX * u + pixelY * v - distance_to_plane * w
@@ -75,24 +71,87 @@ class Scene:
 
                 # TODO: Test for intersection with all objects
 
-                print(r.origin)
-                print(r.direction)
+                # print(r.origin)
+                # print(r.direction)
 
                 for obj in self.objects : 
                     intersection = obj.intersect(r, hc.Intersection(float("inf"), None, (0,0,0), None))
                     
-                    print(intersection.t)
-                    print(intersection.normal)
-                    print(intersection.position)
-                    print(intersection.mat)
+                    # print(intersection.t)
+                    # print(intersection.normal)
+                    # print(intersection.position)
+                    # print(intersection.mat)
                     
+                    # Comment this??
                     if intersection.position != None :  # if there's an intersection found
                         colour = glm.vec3(1, 1, 1)  # color = white
-                    else : 
-                        colour = glm.vec3(0, 0, 0)  # color = black
 
-                # TODO: Perform shading computations on the intersection point
- 
+                        # TODO: Perform shading computations on the intersection point
+                        n = intersection.normal
+                        curPixel = intersection.position
+                        material = intersection.mat
+
+                        print()
+                        print("curPixel : ", curPixel)
+
+                        # Attenuate the light intensity if it's a point light
+                        for light in self.lights : 
+                            if light.type == "point" : 
+                                lightPosition = light.vector
+                                distance = np.linalg.norm(lightPosition - curPixel)    
+                                k_c = light.attenuation[2]
+                                k_l = light.attenuation[1]
+                                k_q = light.attenuation[0]
+
+                                attenuationFactor = 1 / (k_c + k_l * distance + k_q * distance * distance)
+                                I = attenuationFactor * light.colour
+                            else : 
+                                I = light.colour # --> a vector with RGB components
+                        
+
+                        ambientLight = self.ambient
+                        print("I : ", I)
+                        print("ambientLight : " , ambientLight) # ambientLight :  vec3(          0.1,          0.1,          0.1 )
+                        
+                        v = self.eye_position - curPixel    # from the current pixel towards the camera
+                        v = glm.normalize(v)
+                        # print("v : ", v)
+
+                        l = lightPosition - curPixel    # light ray from pixel to light source
+                        l = glm.normalize(l)
+                        # print("l : ", l)
+
+                        # Calculating the Lambertian diffuse shading
+                        k_d = material.diffuse
+                        diffuseLight = k_d * I * max(0, glm.dot(n, l)) 
+                        # print("k_d : ", k_d)    # k_d :  vec3( 1, 0, 0 )
+                        print()
+                        print("n : ", n)
+                        print("l : ", l)
+                        print("glm.dot(n, l): ", glm.dot(n, l))
+                        print("diffuseLight : ", diffuseLight)  # diffuseLight :  vec3(            0,            0,            0 )
+
+                        # k_d * diffuseI is component wise multiplication
+
+                        # Calculating the Blinn-Phong specular shading
+                        p_exponent = material.shininess # 32
+                        k_s = material.specular
+                        h = (v + l) / np.linalg.norm(v + l)     # this is the bissector between v and l
+                        blinnPhongLight = k_s * I * max(0, glm.dot(n, h))** p_exponent
+                        print()
+                        print("n : ", n)
+                        print("h : ", h)
+                        print("glm.dot(n, h): ", glm.dot(n, h))
+                        print("k_s : ", k_s)    # k_s :  vec3(          0.8,          0.8,          0.8 )
+                        print("blinnPhongLight : ", blinnPhongLight)    # blinnPhongLight :  vec3(            0,            0,            0 )
+
+                        colour = ambientLight + diffuseLight + blinnPhongLight
+                        print("colour : ", colour)  # colour :  vec3(          0.1,          0.1,          0.1 ) --> only ambient
+
+                    else : 
+                        # colour = glm.vec3(0, 0, 0)  # color = black
+                        colour = self.ambient
+                    
                 image[row, col, 0] = max(0.0, min(1.0, colour.x))
                 image[row, col, 1] = max(0.0, min(1.0, colour.y))
                 image[row, col, 2] = max(0.0, min(1.0, colour.z))
