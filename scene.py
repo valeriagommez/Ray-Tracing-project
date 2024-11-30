798154ed73e3a554d06bb105984ed330721d4395
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2024-11-29 23:44:16 -0500

    nov 29 - 11:43 pm
    added shadows but they don't work like i want them to -- they shade too abruptly

--- scene.py
+++ scene.py
@@ -52,11 +52,7 @@ class Scene:
         for col in tqdm(range(self.width)):
             for row in range(self.height):
 
-                print("\nSTART OF RAY : ")
-
-                # R = 0
-                # G = 0
-                # B = 0
+                # print("\nSTART OF RAY : ")
 
                 # TODO: Generate rays
                 e = self.eye_position
@@ -65,9 +61,6 @@ class Scene:
                 pixelX = ((col + 0.5)/self.width) * (right - left) + left
                 pixelY = ((row + 0.5)/self.height) * (top - bottom) + bottom
 
-                # print("pixelX : ", pixelX)
-                # print("pixelY : ", pixelY)
-
                 # Calculating the position in 3D of the pixel (in camera coordinates)
                 s = e + pixelX * u + -pixelY * vUnitVector - distance_to_plane * w
                 p = e
@@ -75,32 +68,27 @@ class Scene:
                 d = glm.normalize(d)    # normalizing the direction vector
                 r = hc.Ray(p, d)
 
-                print("p0 of ray : ", p)
-                print("direction of ray : ", d)
+                # print("p0 of ray : ", p)
+                # print("direction of ray : ", d)
 
                 # TODO: Test for intersection with all objects
-
-                # print(r.origin)
-                # print(r.direction)
 
                 intersection = hc.Intersection(float("inf"), None, None, None)
                 closest_t = float("inf")
                 objectRendered = None
 
-                for obj in self.objects : 
-                    print(obj.name)
-                    curIntersection = obj.intersect(r, hc.Intersection(float("inf"), None, None, None))
-                    print("curIntersection.t : ", curIntersection.t)
-                    print("curIntersection.n : ", curIntersection.normal)
+                for sceneObject in self.objects : 
+                    # print(obj.name)
+                    curIntersection = sceneObject.intersect(r, hc.Intersection(float("inf"), None, None, None))
+                    # print("curIntersection.t : ", curIntersection.t)
+                    # print("curIntersection.n : ", curIntersection.normal)
                     
                     if curIntersection.t < closest_t :
                         closest_t = curIntersection.t
-                        print("closest_t : ", closest_t)
+                        # print("closest_t : ", closest_t)
                         intersection = curIntersection
-                        objectRendered = obj
-                        print(obj.name, "was chosen to be rendered!") 
-
-                print("position in 3d of current pixel : ", intersection.position)
+                        objectRendered = sceneObject
+                        # print(obj.name, "was chosen to be rendered!") 
 
                     # Comment this??
                 if intersection.position != None :  # if there's an intersection found
@@ -112,46 +100,69 @@ class Scene:
 
                     v = self.eye_position - curPixel    # from the current pixel towards the camera
                     v = glm.normalize(v)
-                    # print("v : ", v)
 
                     ambientLight = self.ambient
                     diffuseLight = glm.vec3(0, 0, 0)
                     blinnPhongLight = glm.vec3(0, 0, 0)
 
                     for light in self.lights : 
-                        
+                        print("\nlight.name: ", light.name)
+
+                        lightPosition = light.vector
+
                         if light.type == "point" :  # Attenuate the light intensity if it's a point light
-                            lightPosition = light.vector
                             distance = np.linalg.norm(lightPosition - curPixel)    
                             k_c = light.attenuation[2]
                             k_l = light.attenuation[1]
                             k_q = light.attenuation[0]
+                            print("attenuation : ", light.attenuation)
+                            print(k_c)
+                            print(k_l)
+                            print(k_q)
 
                             attenuationFactor = 1 / (k_c + k_l * distance + k_q * distance * distance)
-                            I = attenuationFactor * light.colour # DO I NEED TO MULTIPLY BY POWER??
+                            I = attenuationFactor * light.colour 
                         else : 
                             I = light.colour # --> a vector with RGB components
 
                         l = lightPosition - curPixel    # light ray from pixel to light source
                         l = glm.normalize(l)
                         # print("l : ", l)
-            
-                        # Calculating the Lambertian diffuse shading
-                        k_d = material.diffuse
-                        diffuseLight = diffuseLight + k_d * I * max(0, glm.dot(n, l))
 
-                        # Calculating the Blinn-Phong specular shading
-                        p_exponent = material.shininess # 32
-                        k_s = material.specular
-                        h = (v + l) / np.linalg.norm(v + l)     # this is the bissector between v and l
-                        blinnPhongLight = blinnPhongLight + k_s * I * max(0, glm.dot(n, h)) ** p_exponent
+                        # Doing the shadow rays : 
+
+                        dirToLight = glm.normalize(lightPosition - curPixel)
+                        shadowRay = hc.Ray(curPixel + 0.01 * n , dirToLight)    # adding a bit of offset
+                        inShadow = False
+
+                        for obj in self.objects:
+                            shadowIntersect = obj.intersect(shadowRay, hc.Intersection(float("inf"), None, None, None))
+                            print()
+                            print("objectRendered :", objectRendered.name)
+                            print("obj :", obj.name)
+                            if (shadowIntersect.position != None) :   
+                                print("inShadow!")
+                                # If an interception is found AND we're not intercepting our own object
+                                inShadow = True
+                        
+                        if not inShadow :
+                        # Calculating the Lambertian diffuse shading
+                            k_d = material.diffuse
+                            diffuseLight = diffuseLight + k_d * I * max(0, glm.dot(n, l))
+
+                            # Calculating the Blinn-Phong specular shading
+                            p_exponent = material.shininess # 32
+                            k_s = material.specular
+                            h = (v + l) / np.linalg.norm(v + l)     # this is the bissector between v and l
+                            blinnPhongLight = blinnPhongLight + k_s * I * max(0, glm.dot(n, h)) ** p_exponent
+
+                        # print("blinnPhongLight : " , blinnPhongLight)
+                        # print("diffuseLight : ", diffuseLight)
 
                     colour = ambientLight + diffuseLight + blinnPhongLight
 
                 else : 
-                    colour = glm.vec3(0, 0, 0)  # color = black IN PLANE2 : EVERYTHING BUT SPHERE IS SHOWING UP AS BLACK
-                    # colour = glm.vec3(1, 1, 1)  # color = white
-                    # colour = self.ambient
+                    colour = glm.vec3(0, 0, 0)  # color = black 
                     
                 image[row, col, 0] = max(0.0, min(1.0, colour.x))
                 image[row, col, 1] = max(0.0, min(1.0, colour.y))
